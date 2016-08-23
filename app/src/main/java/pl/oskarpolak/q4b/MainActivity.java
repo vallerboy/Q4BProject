be2edca1e460cf18c682054df05c38024ce8ff19
Vallerboy <10135886>
2016-08-23 17:52:43 +0200

    First commit

---
+++ app/src/main/java/pl/oskarpolak/q4b/MainActivity.java
@@ -0,0 +1,28 @@
+package pl.oskarpolak.q4b;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ScrollView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ScrollView scrollView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scrollView = (ScrollView) findViewById(R.id.scrollView);
+
+    }
+
+    private void loadPicturesIntoViews(){
+        for(int i = 0; i < -)
+    }
+
+
+
+}
